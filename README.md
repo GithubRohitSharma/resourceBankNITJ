b69c5cdcc9ef22732b62b44757bd8c49b6d0d253
Rohit Sharma <rohitsharma2003r@gmail.com>
2025-04-13 08:01:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NITJITResourceBank
+Under Development
+<br>
+"# RESOURCE-BANK-NITJ-IT" 
+"# resource_bank_it" 
+"# resourceBankNITJ" 
+"# resourceBankNITJ" 
---
+++ public/js/login.js
@@ -0,0 +1,54 @@
+function onClickNext(){
+    let form = document.getElementById("loginForm")
+    form.setAttribute("method", "POST")
+    form.setAttribute("action", "/login")
+    form.submit()
+    return false
+}
+
+function onClickForgot(){
+    let form = document.getElementById("loginForm")
+    form.setAttribute("method", "POST")
+    form.setAttribute("action", "/changePassword")
+    form.submit()
+    return false
+}
+
+function main(){
+    let validMail = document.getElementById('error_box_user');
+    let validPass = document.getElementById('error_box_pass');
+    validMail.innerText = "";
+    validPass.innerText = "";
+
+    let problem = document.getElementById("problem").value
+    
+    if(problem == "InvalidPassword"){
+        validPass.innerText = `Invalid password!`;
+        setTimeout(function(){
+            validPass.innerText="";
+            }, 3000);
+    }
+    else if(problem == "InvalidMail"){
+        validMail.innerText = `Enter valid NITJ EMail ID!`;
+        setTimeout(function(){
+            validMail.innerText="";
+            }, 3000);
+    }
+    else if(problem == "UserDNE"){
+        validMail.innerText = `User does not exist! Please SignUp!`;
+        setTimeout(function(){
+            validMail.innerText="";
+            }, 3000);
+    }
+    else if(problem == "WeakPassword"){
+        validPass.innerText = `Choose a stronger blend of characters and numbers!`;
+        setTimeout(function(){
+            validPass.innerText="";
+            }, 3000);
+    }
+    
+    (document.getElementById('next')).onclick = () => onClickNext();
+    (document.getElementById('FGPassword')).onclick = () => onClickForgot();
+}
+
+main()
